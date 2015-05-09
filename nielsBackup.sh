f916ef556b0d403245a44f1a3812a7009a798193
mulderniels <mulderniels@users.noreply.github.com>
2015-05-09 23:22:55 +0200

    Work with vars
    Work with vars for database, objectstore and paths to backup

--- nielsBackup.sh
+++ nielsBackup.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+#path to temp backup place
 cd /var/nielsBackup
 
-mysqldump -u xxx --password=xxx --all-databases | gzip --best > sql-backup-$(date +%Y-%m-%d-%H-%M-%S).sql.zip
-tar -czf www-backup-$(date +%Y-%m-%d-%H-%M-%S).zip /var/www /etc/nginx
+#settings
+dbUser="xxx"
+dbPass="xxx"
+
+toBack="/var/www /etc/nginx" #paths to backup, space separated
+
+osUser="xxx"
+osPass="xxx"
+osPath="https://xxx.objectstore.eu/backups"
+
+
+#do things
+filenameDate=$(date +%Y-%m-%d-%H-%M-%S)
+
+mysqldump -u $dbUser --password=$dbPass --all-databases | gzip --best > sql-backup-$filenameDate.sql.zip
+tar -czf www-backup-$filenameDate.zip $toBack
 
 for file in *.zip; do
-	curl -X PUT -T $file --user xxx:xxx https://xxx.objectstore.eu/backups/$file
+	curl -X PUT -T $file --user $osUser:$osPass $osPath/$file
 done
 
 rm *.zip
