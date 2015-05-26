9788155cb028f6055d33670c92ecf3ff3467b8ab
mulderniels <mulderniels@users.noreply.github.com>
2015-05-26 11:01:31 +0200

    added remove old backups on objectstore
    added remove old backups on objectstore

--- nielsBackup.sh
+++ nielsBackup.sh
@@ -13,15 +13,27 @@ osUser="xxx"
 osPass="xxx"
 osPath="https://xxx.objectstore.eu/backups"
 
+maxHours=100
 
-#do things
+#create new backups
 filenameDate=$(date +%Y-%m-%d-%H-%M-%S)
+maxDate=$(date +%Y%m%d%H%M%S -d "${maxHours} hours ago")
 
 mysqldump -u $dbUser --password=$dbPass --all-databases | gzip --best > sql-backup-$filenameDate.sql.zip
 tar -czf www-backup-$filenameDate.zip $toBack
 
+#upload new backups to objectstore
 for file in *.zip; do
-	curl -X PUT -T $file --user $osUser:$osPass $osPath/$file
+	curl -X PUT -s -T $file --user $osUser:$osPass $osPath/$file
 done
 
+#remove new backups locally
 rm *.zip
+
+#remove old backups on objectstore
+while read fileName
+do
+	if [ "${fileName//[!0-9]/}" -lt $maxDate ]; then
+		curl -X DELETE -s --user $osUser:$osPass $osPath/$fileName
+	fi
+done  < <(curl -s --user $osUser:$osPass $osPath)
